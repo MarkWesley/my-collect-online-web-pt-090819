be884b52a344ffc9f5198eb2b6de6bb8edb145da
Mark Stone <markwstone85@gmail.com>
2019-09-20 02:14:09 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@ def my_collect(collection)
   counter = 0 
   empty_array = []
   while  
-    yield collection[languages]
-    i =+ 1
+    yield collection[counter]
+    counter += 1
   end
   collection
 end
